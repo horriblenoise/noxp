0902f762446e01ffb9094e97bde1cbd37be6e258
alexu <alexstickley90@gmail.com>
2021-03-04 22:09:18 -0500

    initial commit

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+   ourRigidBody.velocity = transform.forward * bulletSpeed;
+    }
+}
---
+++ Assets/PlayerBehaviour2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehaviour2 : MonoBehaviour {
+    public float speed;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        ourRigidBody.velocity = inputVector * speed;
+       
+        Vector3 LookAtPosition = transform.position + inputVector;
+        transform.LookAt(LookAtPosition);
+
+       if (Input.GetButton("Fire1"))
+       {
+           Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
+       }
+    }
+}
